d77e59153fb221d077ce7c852a8c52c13e6c751d
KrunalDattatech <kbeladiya@dattatechnologies.com>
2024-02-11 02:14:05 +0530

    add gitignore

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import userRoutes from "./routes/user.routes.js";
 import customerRoutes from "./routes/customer.routes.js";
 import courseRoutes from "./routes/course.routes.js";
@@ -11,6 +10,7 @@ import mediaRoutes from './routes/media.routes.js'
 const URL = process.env.HOST_URL;
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8000;
+
 const app = express();
 
 app.use(
@@ -36,6 +36,10 @@ app.use("/api/user", userRoutes);
 app.use("/api/customer", customerRoutes);
 app.use("/api/course", courseRoutes);
 app.use('/api/storage/public/images', mediaRoutes)
+
+// app.listen(8800, () => {
+//   console.log("Backend server is running!");
+// });
 
 app.listen(PORT, HOST, () => {
   console.log(`Api Listenig at ${URL}:${PORT}`);
